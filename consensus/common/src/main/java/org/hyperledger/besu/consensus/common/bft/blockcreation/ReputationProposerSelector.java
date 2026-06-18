97eeedaccfe6292b21ecba6f360fafbcf6aaf758
nooresaba26 <nooresaba61@gmail.com>
2026-06-19 00:01:14 +0600

    ReputationScoreCalculator.java and ReputationProposerSelector.java

--- consensus/common/src/main/java/org/hyperledger/besu/consensus/common/bft/blockcreation/ReputationProposerSelector.java
+++ consensus/common/src/main/java/org/hyperledger/besu/consensus/common/bft/blockcreation/ReputationProposerSelector.java
@@ -1,80 +1,40 @@
-/*
- * Copyright contributors to Besu.
- *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
- * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
- * specific language governing permissions and limitations under the License.
- *
- * SPDX-License-Identifier: Apache-2.0
- */
 package org.hyperledger.besu.consensus.common.bft.blockcreation;
 
-import static com.google.common.base.Preconditions.checkArgument;
-
-import org.hyperledger.besu.consensus.common.bft.ConsensusRoundIdentifier;
 import org.hyperledger.besu.consensus.common.validator.ValidatorProvider;
 import org.hyperledger.besu.datatypes.Address;
 import org.hyperledger.besu.ethereum.chain.Blockchain;
-import org.hyperledger.besu.ethereum.core.BlockHeader;
 
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
-import java.util.Optional;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class ReputationProposerSelector implements ProposerSelector {
 
-  private static final Logger LOG = LoggerFactory.getLogger(ReputationProposerSelector.class);
-
   private final Blockchain blockchain;
-  // private final BlockInterface blockInterface;
   private final ValidatorProvider validatorProvider;
 
   public ReputationProposerSelector(
       final Blockchain blockchain,
-      // final BlockInterface blockInterface,
       final ValidatorProvider validatorProvider) {
     this.blockchain = blockchain;
-    // this.blockInterface = blockInterface;
     this.validatorProvider = validatorProvider;
   }
 
   @Override
-  public Address selectProposerForRound(final ConsensusRoundIdentifier roundIdentifier) {
-    checkArgument(roundIdentifier.getRoundNumber() >= 0);
-    checkArgument(roundIdentifier.getSequenceNumber() > 0);
+  public Address selectProposerForRound(final int round) {
+    final Collection<Address> validators =
+        validatorProvider.getValidatorsAfterBlock(blockchain.getChainHeadHeader());
 
-    final long parentBlockNumber = roundIdentifier.getSequenceNumber() - 1;
-    final Optional<BlockHeader> maybeParentHeader = blockchain.getBlockHeader(parentBlockNumber);
+    final List<Address> committee = new ArrayList<>(validators);
+    committee.sort(Address::compareTo);
 
-    if (maybeParentHeader.isEmpty()) {
-      LOG.trace("Unable to determine proposer for requested block {}", parentBlockNumber);
-      throw new RuntimeException("Unable to determine parent block for reputation proposer");
+    if (committee.isEmpty()) {
+      throw new IllegalStateException("Reputation committee cannot be empty");
     }
 
-    final BlockHeader parentHeader = maybeParentHeader.get();
-    final Collection<Address> validatorsForRound =
-        validatorProvider.getValidatorsAfterBlock(parentHeader);
+    final int index =
+        Math.floorMod((int) blockchain.getChainHeadHeader().getNumber() + round, committee.size());
 
-    if (validatorsForRound.isEmpty()) {
-      throw new RuntimeException("No validators available for reputation proposer selection");
-    }
-
-    final List<Address> sortedValidators = new ArrayList<>(validatorsForRound);
-    sortedValidators.sort(null);
-
-    final long rotation = roundIdentifier.getSequenceNumber() + roundIdentifier.getRoundNumber();
-
-    final int proposerIndex = (int) Math.floorMod(rotation, (long) sortedValidators.size());
-
-    return sortedValidators.get(proposerIndex);
+    return committee.get(index);
   }
 }
--- consensus/qbft/src/main/java/org/hyperledger/besu/consensus/qbft/validator/ReputationScoreCalculator.java
+++ consensus/qbft/src/main/java/org/hyperledger/besu/consensus/qbft/validator/ReputationScoreCalculator.java
@@ -6,31 +6,32 @@ import org.hyperledger.besu.ethereum.core.BlockHeader;
 public class ReputationScoreCalculator {
 
   private final ReputationSelectionConfig config;
+  private final ValidatorMetricsProvider metricsProvider;
   private final ParticipationBalanceTracker participationBalanceTracker;
 
   public ReputationScoreCalculator(
       final ReputationSelectionConfig config,
+      final ValidatorMetricsProvider metricsProvider,
       final ParticipationBalanceTracker participationBalanceTracker) {
     this.config = config;
+    this.metricsProvider = metricsProvider;
     this.participationBalanceTracker = participationBalanceTracker;
   }
 
   public double calculateScore(final Address validator, final BlockHeader parentHeader) {
-    final double uptime = 1.0;
-    final double successRate = 1.0;
-    final double failureRate = 0.0;
+    final double uptime = metricsProvider.uptime(validator, parentHeader);
+    final double successRate = metricsProvider.successRate(validator, parentHeader);
+    final double failureRate = metricsProvider.failureRate(validator, parentHeader);
 
     final double baseScore =
         config.getAlpha() * uptime
             + config.getBeta() * successRate
             + config.getGamma() * (1.0 - failureRate);
 
-    final double timeDecayedScore = baseScore;
-
     final double participationBalance =
         participationBalanceTracker.participationBalance(validator, parentHeader);
 
-    return clamp(timeDecayedScore * participationBalance);
+    return clamp(baseScore * participationBalance);
   }
 
   private double clamp(final double value) {
